ff54b48df4de43f1ce6e3c5c14d182280b91edda
Nessaa16 <120008781+Nessaa16@users.noreply.github.com>
2026-02-26 21:32:12 +0700

    fix: missing home about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/Header";
-import { AboutSection } from "@/components/AboutSection";
+import { HomeAboutSection } from "@/components/HomeAboutSection";
 import { ExecutiveProfileSection } from "@/components/ExecutiveProfileSection";
 import { CardAboutSection } from "@/components/CardAboutSection";
 import { ExploreWorkSection } from "@/components/ExploreWorkSection";
@@ -9,7 +9,7 @@ export default function AboutPage() {
   return (
     <main className="relative w-full overflow-x-hidden text-sm text-zinc-200">
       <Header />
-      <AboutSection />
+      <HomeAboutSection />
       <ExecutiveProfileSection />
       <CardAboutSection />
       <ExploreWorkSection />
