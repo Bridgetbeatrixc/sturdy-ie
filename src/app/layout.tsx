eee75fbe995c6d6f79fa6c157ffb2d9922d10c5c
Nessaa16 <120008781+Nessaa16@users.noreply.github.com>
2026-02-26 17:42:28 +0700

    refactor: change default bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased bg-[radial-gradient(circle_at_center,rgba(197,240,24,0.2),transparent_70%)] bg-neutral-950 text-zinc-100`}
+        className={`${poppins.className} antialiased bg-black text-zinc-100`}
       >
         <div className="flex min-h-screen min-w-0 justify-center">
           {children}
